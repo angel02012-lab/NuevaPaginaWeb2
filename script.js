750967278924503e6c679575e62fed34511cbcac
angel javier <aroblescerritos@gmail.com>
2024-12-29 20:50:41 -0800

    para ver si funciona

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let media 
-let array
-let sum 
+let add
+let sum
+let arr 
 
-function arr() {
-   sum = 0
-   for (let i = 0; i < array.length; i++) {
-    sum += array[i] 
-   } 
-   media = sum / array.length
+function arrs() {
+     sum = 0
+    for(let i = 0; i < arr.length; i++) {
+    sum += arr[i]       
+    }
+    add = sum / arr.length
 }
 
-array = [10,20,40,40]
-arr()
-console.log(media)
-   
+arr = [10,20,30,40] 
+arrs()
+console.log(add)
+
